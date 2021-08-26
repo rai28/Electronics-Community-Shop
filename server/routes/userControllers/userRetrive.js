6434289ca0ad1bb9025cfad0e09e303c3d06c725
Ritesh Rai <riteshrai997@gmail.com>
2021-08-26 23:56:57 +0530

    userRetrive API in userRouter

--- server/routes/userControllers/userRetrive.js
+++ server/routes/userControllers/userRetrive.js
@@ -2,6 +2,8 @@ const express = require("express");
 const expressAsyncHandler = require("express-async-handler");
 const User = require("../../models/user");
 const userRetrive = express.Router();
+const bcrypt = require("bcryptjs");
+const generateToken = require("../../middlewares/jwtVerify");
 const isAuth = require("../../middlewares/isAuth");
 
 userRetrive.get(
@@ -15,6 +17,32 @@ userRetrive.get(
       });
     }
     res.status(200).json(user);
+  })
+);
+
+userRetrive.put(
+  "/profile",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    user.name = req.body.userName || user.userName;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await user.save();
+    res.send({
+      id: updatedUser._id,
+      name: updatedUser.userName,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      data: generateToken(updatedUser),
+    });
   })
 );
 
